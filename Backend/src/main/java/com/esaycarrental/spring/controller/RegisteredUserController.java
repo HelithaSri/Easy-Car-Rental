d40dfa0311cab03f7e25fe1f887a61d0b6e4f5a4
HelithaSri <helitha.pravin@gmail.com>
2022-07-07 18:25:19 +0530

    Create Controller package

---
+++ Backend/src/main/java/com/esaycarrental/spring/controller/RegisteredUserController.java
@@ -0,0 +1,18 @@
+package com.esaycarrental.spring.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Helitha Sri
+ * @created 7/6/2022 - 11:25 PM
+ * @project Easy Car Rental
+ */
+
+@RestController
+@RequestMapping("api/user")
+@CrossOrigin
+public class RegisteredUserController {
+
+}
