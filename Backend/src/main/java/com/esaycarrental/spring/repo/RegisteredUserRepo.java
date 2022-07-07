7a8e44864275b5685b88bc640cb1e6c2915aca1d
HelithaSri <helitha.pravin@gmail.com>
2022-07-07 18:42:16 +0530

    Create Packages

---
+++ Backend/src/main/java/com/esaycarrental/spring/repo/RegisteredUserRepo.java
@@ -0,0 +1,11 @@
+package com.esaycarrental.spring.repo;
+
+/**
+ * @author Helitha Sri
+ * @created 7/7/2022 - 6:40 PM
+ * @project Easy Car Rental
+ */
+
+public interface RegisteredUserRepo {
+
+}
---
+++ Backend/src/main/java/com/esaycarrental/spring/service/RegisteredUserService.java
@@ -0,0 +1,10 @@
+package com.esaycarrental.spring.service;
+
+/**
+ * @author Helitha Sri
+ * @created 7/7/2022 - 6:41 PM
+ * @project Easy Car Rental
+ */
+
+public class RegisteredUserService {
+}
