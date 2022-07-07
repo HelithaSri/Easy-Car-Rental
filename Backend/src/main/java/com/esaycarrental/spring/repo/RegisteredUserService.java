2c9a8695c2601ee088f9b77d10c876b2967c0f06
HelithaSri <helitha.pravin@gmail.com>
2022-07-07 18:24:56 +0530

    Create Repo package

---
+++ Backend/src/main/java/com/esaycarrental/spring/repo/RegisteredUserService.java
@@ -0,0 +1,11 @@
+package com.esaycarrental.spring.repo;
+
+/**
+ * @author Helitha Sri
+ * @created 7/6/2022 - 11:26 PM
+ * @project Easy Car Rental
+ */
+
+public interface RegisteredUserService {
+
+}
---
+++ Backend/src/main/java/com/esaycarrental/spring/repo/impl/RegisteredUserServiceImpl.java
@@ -0,0 +1,12 @@
+package com.esaycarrental.spring.repo.impl;
+
+import com.esaycarrental.spring.repo.RegisteredUserService;
+
+/**
+ * @author Helitha Sri
+ * @created 7/6/2022 - 11:26 PM
+ * @project Easy Car Rental
+ */
+
+public class RegisteredUserServiceImpl implements RegisteredUserService {
+}
