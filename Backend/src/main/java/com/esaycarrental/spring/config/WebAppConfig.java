e13d78f5b86cbc711c75bc82db1ab282d3b0def3
HelithaSri <helitha.pravin@gmail.com>
2022-07-08 09:07:36 +0530

    Added modelmapper

--- Backend/src/main/java/com/esaycarrental/spring/config/WebAppConfig.java
+++ Backend/src/main/java/com/esaycarrental/spring/config/WebAppConfig.java
@@ -1,5 +1,7 @@
 package com.esaycarrental.spring.config;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -14,4 +16,9 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @Configuration
 @ComponentScan(basePackages = "com.esaycarrental.spring")
 public class WebAppConfig {
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
 }
