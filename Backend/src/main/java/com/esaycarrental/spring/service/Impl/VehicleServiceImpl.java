bc2c149033b4c42b7a4558938cdbd3c2bdda363b
HelithaSri <helitha.pravin@gmail.com>
2022-07-08 18:25:04 +0530

    set vehicle type and rates object to saved vehicleDTO

--- Backend/src/main/java/com/esaycarrental/spring/service/Impl/VehicleServiceImpl.java
+++ Backend/src/main/java/com/esaycarrental/spring/service/Impl/VehicleServiceImpl.java
@@ -1,9 +1,13 @@
 package com.esaycarrental.spring.service.Impl;
 
+import com.esaycarrental.spring.dto.RatesDTO;
 import com.esaycarrental.spring.dto.VehicleDTO;
+import com.esaycarrental.spring.dto.VehicleTypeDTO;
 import com.esaycarrental.spring.entity.Rates;
 import com.esaycarrental.spring.entity.Vehicle;
+import com.esaycarrental.spring.repo.RatesRepo;
 import com.esaycarrental.spring.repo.VehicleRepo;
+import com.esaycarrental.spring.repo.VehicleTypeRepo;
 import com.esaycarrental.spring.service.VehicleService;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
@@ -12,6 +16,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * @author Helitha Sri
@@ -29,9 +34,31 @@ public class VehicleServiceImpl implements VehicleService {
     @Autowired
     private ModelMapper mapper;
 
+    @Autowired
+    private RatesRepo ratesRepo;
+
+    @Autowired
+    private VehicleTypeRepo vehicleTypeRepo;
+
     @Override
     public void saveVehicle(VehicleDTO vehicleDTO) {
         if (!repo.existsById(vehicleDTO.getRegistrationNumber())){
+
+            if (vehicleTypeRepo.existsById(vehicleDTO.getType().getVehicleTypeId())){
+                VehicleTypeDTO type = mapper.map(vehicleTypeRepo.findByVehicleTypeId(vehicleDTO.getType().getVehicleTypeId()),VehicleTypeDTO.class);
+                vehicleDTO.setType(type);
+            }else {
+                throw new RuntimeException("Please Check the Vehicle Type ID");
+            }
+
+            if (ratesRepo.existsById(vehicleDTO.getRates().getRateId())){
+                RatesDTO rates = mapper.map(ratesRepo.findByRateId(vehicleDTO.getRates().getRateId()),RatesDTO.class);
+                vehicleDTO.setRates(rates);
+            }else {
+                throw new RuntimeException("Please Check the RateID");
+            }
+
+//            System.out.println("\nchange : "+ vehicleDTO+"\n");
             repo.save(mapper.map(vehicleDTO, Vehicle.class));
         }else {
             throw new RuntimeException("User Already Exist");
