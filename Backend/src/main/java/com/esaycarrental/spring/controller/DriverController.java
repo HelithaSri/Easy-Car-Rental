82d424e7d67683f4e9748377aaae764b72e3e9d4
HelithaSri <helitha.pravin@gmail.com>
2022-07-09 16:20:56 +0530

    Basic CRUD Done

---
+++ Backend/src/main/java/com/esaycarrental/spring/controller/DriverController.java
@@ -0,0 +1,49 @@
+package com.esaycarrental.spring.controller;
+
+import com.esaycarrental.spring.dto.DriverDTO;
+import com.esaycarrental.spring.service.DriverService;
+import com.esaycarrental.spring.util.ResponseUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @author Helitha Sri
+ * @created 7/9/2022 - 3:14 PM
+ * @project Easy Car Rental
+ */
+
+@RestController
+@RequestMapping("api/driver")
+@CrossOrigin
+public class DriverController {
+
+    @Autowired
+    DriverService service;
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil getAllDrivers(){
+        return new ResponseUtil(200,"Ok",service.getAllDrivers());
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil saveDriver(@ModelAttribute DriverDTO driverDTO){
+        service.saveDriver(driverDTO);
+        return new ResponseUtil(200, "New Driver Added Successfully", null);
+    }
+
+    @DeleteMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil deleteDriver(@RequestParam String driverId){
+        service.deleteDriver(driverId);
+        return new ResponseUtil(200, "Driver Delete Successfully", null);
+    }
+
+    public ResponseUtil updateDriver(DriverDTO driverDTO){
+        service.updateDriver(driverDTO);
+        return new ResponseUtil(200, "Driver Update Successfully", null);
+    }
+
+
+}
