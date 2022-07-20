46dff542eecd6d2aed1cc6954d597eb6794c14e8
HelithaSri <helitha.pravin@gmail.com>
2022-07-20 13:01:25 +0530

    div => Grid

--- frontend/src/app/App.js
+++ frontend/src/app/App.js
@@ -1,16 +1,16 @@
 import Home from "../pages/Home";
 import Typography from "@mui/material/Typography";
 import ListVehicle from "../components/ListVehicle";
+import { Grid } from "@mui/material";
 
 
 function App() {
   return (
-    <div>
+    <Grid container>
       <Home/>
-      {/* <CommonCard/> */}
-      <ListVehicle/>
       
-    </div>
+      
+    </Grid>
   );
 }
 
