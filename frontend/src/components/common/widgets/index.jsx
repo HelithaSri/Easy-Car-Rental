f1faf1f7997f7909ee33711381b0473e1e0bf9a6
HelithaSri <helitha.pravin@gmail.com>
2022-07-23 05:11:10 +0530

    Create widget

---
+++ frontend/src/components/common/widgets/index.jsx
@@ -0,0 +1,13 @@
+import { Grid } from '@mui/material'
+import React, { Component } from 'react'
+
+class Widget extends Component {
+  render() {
+    return (
+      <Grid>
+        
+      </Grid>
+    )
+  }
+}
+export default Widget
