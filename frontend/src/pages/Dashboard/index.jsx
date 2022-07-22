0f290ab2f962622b3f325d4fa025ae024530492b
HelithaSri <helitha.pravin@gmail.com>
2022-07-23 05:11:40 +0530

    added widgets

--- frontend/src/pages/Dashboard/index.jsx
+++ frontend/src/pages/Dashboard/index.jsx
@@ -1,13 +1,20 @@
 import { Grid } from '@mui/material'
 import React, { Component } from 'react'
+import Navbar from '../../components/common/Navbar/Admin'
 import Sidebar from '../../components/common/Sidebar'
+import Widget from '../../components/common/widgets'
 
 class Dashboard extends Component {
   render() {
     return (
       <Grid container direction={'row'} columns='12'>
         <Grid item xs={'auto'} ><Sidebar/></Grid>
-        <Grid item xs className='bg-red-100'>cons</Grid>
+        <Grid item xs className='bg-red-100'>
+          <Navbar/>
+          <Grid item>
+            <Widget/>
+          </Grid>
+          </Grid>
       </Grid>
     )
   }
