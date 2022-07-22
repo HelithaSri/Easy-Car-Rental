1722e5ac1099069d5a975c10c80ce0dc4bca9afa
HelithaSri <helitha.pravin@gmail.com>
2022-07-23 00:53:24 +0530

    Use Grids

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,30 +1,31 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 import NavBar from "../../components/NavBar";
-import Hero from "../../components/Hero/withFlex";
-import HeroWithGrid from '../../components/Hero/withGrid';
-import ListVehicle from '../../components/ListVehicle';
-import Footer from '../../components/Footer';
-import LoginUser from '../Session/Login/user';
-import CustomizedDialogs from '../../components/common/Dialog';
-import LoginAdmin from '../Session/Login/admin';
-import LoginDriver from '../Session/Login/driver';
+import HeroWithGrid from "../../components/Hero/withGrid";
+import ListVehicle from "../../components/ListVehicle";
+import Footer from "../../components/Footer";
+import LoginUser from "../Session/Login/user";
+import LoginAdmin from "../Session/Login/admin";
+import LoginDriver from "../Session/Login/driver";
+import { Grid } from "@mui/material";
 
 class Home extends Component {
-    render() {
-        return (
-            // overflowX:'hidden'
-            <div style={{display:'flex', flexDirection:'column'}}>
-                <NavBar/>
-                <HeroWithGrid/>
-                <ListVehicle/>
-                <Footer/>
-                {/* <LoginUser/>  */}
-               
-                {/* <LoginAdmin/>
+  render() {
+    return (
+      // overflowX:'hidden'
+      <Grid container>
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <NavBar />
+          <HeroWithGrid />
+          <ListVehicle />
+          <Footer />
+          {/* <LoginUser/>  */}
+ 
+          {/* <LoginAdmin/>
                 <LoginDriver/> */}
-            </div>
-        );
-    }
+        </div>
+      </Grid>
+    );
+  }
 }
 
 export default Home;
