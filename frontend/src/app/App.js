c1fd8bdc51f6e5e5d4f559dc9d063ae8ded9d222
HelithaSri <helitha.pravin@gmail.com>
2022-07-24 05:29:23 +0530

    vehicle done

--- frontend/src/app/App.js
+++ frontend/src/app/App.js
@@ -1,37 +1,40 @@
 import Home from "../pages/Home";
-import { Route, Routes } from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import Dashboard from "../pages/Admin/Dashboard";
 import NotFound from '../pages/Session/NotFound'
 import LoginAdmin from "../pages/Session/Login/admin";
 import LoginDriver from "../pages/Session/Login/driver";
-import LoginUser from "../pages/Session/Login/user";
 import VehicleManage from "../pages/Admin/Vehicle Manage";
 import AddNewVehicle from "../components/AddVehicle";
+import VehicleType from "../pages/Admin/VehicleTypes";
+import VehicleRates from "../pages/Admin/VehicleRates";
 
 
 function App() {
-  return (
-    <Routes>
-      <Route exact path="/" element={<Home />} />
-      <Route path="dashboard" element={<Dashboard />} />
+    return (
+        <Routes>
+            <Route exact path="/" element={<Home/>}/>
+            <Route path="dashboard" element={<Dashboard/>}/>
 
-      <Route path="admin">
-        <Route index element={<LoginAdmin />} />
-        <Route path="dashboard">
-          <Route index element={<Dashboard />} />
-          <Route path="vehiclemanage" element={<VehicleManage />} />
-          <Route path="test" element={<AddNewVehicle />} />
-        </Route>
-      </Route>
+            <Route path="admin">
+                <Route index element={<LoginAdmin/>}/>
+                <Route path="dashboard">
+                    <Route index element={<Dashboard/>}/>
+                    <Route path="vehiclemanage" element={<VehicleManage/>}/>
+                    <Route path="vehiclerates" element={<VehicleRates/>}/>
+                    <Route path="vehicletype" element={<VehicleType/>}/>
+                    <Route path="test" element={<AddNewVehicle/>}/>
+                </Route>
+            </Route>
 
-      <Route path="driver">
-        <Route index element={<LoginDriver />} />
-        <Route path="dashboard" element={<Dashboard />} />
-      </Route>
+            <Route path="driver">
+                <Route index element={<LoginDriver/>}/>
+                <Route path="dashboard" element={<Dashboard/>}/>
+            </Route>
 
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-  );
+            <Route path="*" element={<NotFound/>}/>
+        </Routes>
+    );
 }
 
 export default App;
