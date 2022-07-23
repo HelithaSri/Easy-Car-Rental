6bafb9eaf5fa688660e7d1158435c9eb3dcad6b4
HelithaSri <helitha.pravin@gmail.com>
2022-07-23 15:41:10 +0530

    config routes

--- frontend/src/app/App.js
+++ frontend/src/app/App.js
@@ -2,14 +2,28 @@ import Home from "../pages/Home";
 import { Route, Routes } from "react-router-dom";
 import Dashboard from "../pages/Dashboard";
 import NotFound from '../pages/Session/NotFound'
+import LoginAdmin from "../pages/Session/Login/admin";
+import LoginDriver from "../pages/Session/Login/driver";
+import LoginUser from "../pages/Session/Login/user";
 
 
 function App() {
   return (
     <Routes>
       <Route exact path="/" element={<Home />} />
-      <Route path="admin" element={<Dashboard />} />
-      <Route path="*" element={<NotFound />} />
+      <Route path="dashboard" element={<Dashboard />} />
+      
+      <Route path="admin">
+        <Route index element={<LoginAdmin />} />
+        <Route path="dashboard" element={<Dashboard />} />
+      </Route>
+
+      <Route path="driver">
+        <Route index element={<LoginDriver />} />
+        <Route path="dashboard" element={<Dashboard />} />
+      </Route>
+
+        <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
